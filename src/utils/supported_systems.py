29f4116ffeb9b977180ec4d5aabb3ceb6ab725df
Mijux <mijux-dev@proton.me>
2023-11-06 09:58:27 +0100

    add custom exceptions

--- src/utils/supported_systems.py
+++ src/utils/supported_systems.py
@@ -2,6 +2,7 @@
 
 from os import listdir
 from os.path import join, abspath, exists, isdir
+from utils.custom_exceptions import PathDoesNotExist, VariablePathNotDefined
 from yaml import safe_load
 
 class SingletonSupportedSystems():
@@ -12,7 +13,8 @@ class SingletonSupportedSystems():
             class_._instances[class_] = super(SingletonSupportedSystems, class_).__new__(class_, *args, **kwargs)
         return class_._instances[class_]
 
-
+class PathException(Exception):
+    pass
 class SupportedSystems(SingletonSupportedSystems):
     _playbooks_location = ""
     _os_selected = ""
@@ -32,42 +34,44 @@ class SupportedSystems(SingletonSupportedSystems):
             if exists(p) and isdir(p):
                 return p
             else:
-                print("Raise an exception M")
+                raise PathDoesNotExist(f"The following path does not exist : {p}")
         else:
-            print("Raise an exception N")
+            raise VariablePathNotDefined(f"Variables _playbooks_location, _os_selected, _os_type_selected and _os_version_selected must be filled")
 
     def set_playbooks_location(self, path : str) -> list[str]:
         if len(path) > 0:
             if exists(path):
                 self._playbooks_location = path
             else:
-                print("TODO : raise an exception A")
+                raise PathDoesNotExist(f"The following path does not exist : {path}")
         else:
-            print("TODO : raise an exception B")
+            raise VariablePathNotDefined("The path must be a string with len > 0")
 
     def list_os(self) -> list[str]:
         if len(self._playbooks_location) <= 0:
-            print("TODO : raise an exception C")
+            raise VariablePathNotDefined(f"Variables _playbooks_location musts be filled")
         else:
             return listdir(self._playbooks_location)
 
     def list_os_type(self) -> list[str]:
         if len(self._playbooks_location) <= 0 or len(self._os_selected) <= 0:
-            print("TODO : raise an exception D")
+            raise VariablePathNotDefined(f"Variables _playbooks_location and _os_selected must be filled")
         else:
-            if not exists(join(self._playbooks_location,self._os_selected)):
+            p = join(self._playbooks_location,self._os_selected)
+            if not exists(p):
                 self.reset_params()
-                print("TODO : raise an exception E")
+                raise PathDoesNotExist(f"The following path does not exist : {p}")
             else:
                 return listdir(join(self._playbooks_location,self._os_selected))
 
     def list_os_version(self) -> list[str]:
         if len(self._playbooks_location) <= 0 or len(self._os_selected) <= 0 or len(self._os_type_selected) <= 0 :
-            print("TODO : raise an exception F")
+            raise VariablePathNotDefined(f"Variables _playbooks_location and _os_selected, _os_type_selected must be filled")
         else:
-            if not exists(join(self._playbooks_location,self._os_selected,self._os_type_selected)):
+            p = join(self._playbooks_location,self._os_selected,self._os_type_selected)
+            if not exists(p):
                 self.reset_params()
-                print("TODO : raise an exception G")
+                raise PathDoesNotExist(f"The following path does not exist : {p}")
             else:
                 return listdir(join(self._playbooks_location,self._os_selected, self._os_type_selected))
 
@@ -75,22 +79,22 @@ class SupportedSystems(SingletonSupportedSystems):
         if len(os) >= 1:
             self._os_selected = os.upper()
         else:
-            print("TODO : raise an exception H")
+            raise VariablePathNotDefined(f"The variable os must be a string with len > 0")
 
     def select_os_type(self, os_type : str) -> None:
         if len(self._os_selected) >= 1: 
             if len(os_type) >= 1:
                 self._os_type_selected = os_type.upper()
             else:
-                print("TODO : raise an exception I")
+                raise VariablePathNotDefined(f"The variable os_type must be a string with len > 0")
         else:
-            print("TODO : raise an exception J")
+            raise VariablePathNotDefined(f"The variable _os_selected cannot be a string with len == 0")
 
     def select_os_version(self, os_version : str) -> None:
         if len(self._os_selected) >=1 and len(self._os_type_selected) >= 1: 
             if len(os_version) >= 1:
                 self._os_version_selected = os_version.upper()
             else:
-                print("TODO : raise an exception K")
+                raise VariablePathNotDefined(f"The variable os_version must be a string with len > 0")
         else:
-            print("TODO : raise an exception L")
+            raise VariablePathNotDefined(f"The variables _os_selected and _os_type_selected cannot be string with len == 0")
