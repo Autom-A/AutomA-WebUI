16eb0183fa724366ea1b7201aa097e38f23738a4
Mijux <mijux-dev@proton.me>
2023-11-14 23:29:00 +0100

    CSS theming + Requests Front/Back + OS Selector

--- src/main.py
+++ src/main.py
@@ -1,38 +1,112 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route("/")
-# def status():
-#     return "<h1>Server is Running</h1>"
-
-
-# app.run(host="127.0.01", port="9123", debug=True)
-
-from utils.supported_systems import SupportedSystems
-from utils.questions_parser import read_questions_file, list_categories, list_anssi_recommandations
+from flask import Flask, render_template, jsonify, request
+from flask_cors import CORS
 from os.path import abspath, join
+from utils.custom_exceptions import PathDoesNotExist, VariablePathNotDefined
+from utils.questions_parser import list_anssi_recommandations, list_categories
+from utils.supported_systems import SupportedSystems
 
-ss = SupportedSystems()
-ss.set_playbooks_location(join(abspath("."),"playbooks"))
+STATIC_FOLDER = join(abspath("."),"ressources","static")
+flask_app = Flask(__name__, static_folder=STATIC_FOLDER)
+CORS(flask_app)
 
-ossup = ss.list_os()
-print(f"OS supported : {ossup}")
-ss.select_os(input("Select OS > "))
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = "9123"
 
-ostypesup = ss.list_os_type()
-print(f"OS type supported : {ostypesup}")
-ss.select_os_type(input("Select OS type > "))
+supported_systems = SupportedSystems()
+supported_systems.set_playbooks_location("./playbooks")
 
-versionsup = ss.list_os_version()
-print(f"OS version supported : {versionsup}")
-ss.select_os_version(input("Select OS version > "))
+@flask_app.route("/")
+def index():
+    return render_template("index.html")
 
-print(list_categories())
-
-cat = input("Choose Category > ")
-print(list_anssi_recommandations(cat))
+@flask_app.route("/app")
+def app():
+    return render_template("app.html",SERVER_IP=SERVER_IP, SERVER_PORT=SERVER_PORT)
 
 
-p = join(ss.get_entire_path(),cat.upper(),"ANSSI","1_intermediate","Rxx_Audit_Admin_Actions","questions.yml")
-print(read_questions_file(p))
+@flask_app.route("/api/selector/os", methods=['GET'])
+def get_os():
+    try:
+        return jsonify(supported_systems.get_os())
+    except VariablePathNotDefined as e:
+        return jsonify({"ERROR":e.args}), 400
+
+@flask_app.route("/api/selector/os", methods=['POST'])
+def post_os():
+    try:
+        os = request.get_json().get("os")
+        if not os:
+            return jsonify({"ERROR":"os variable not found"})
+        else:
+            supported_systems.set_os(os)
+            return jsonify({"SUCCESS":"os has been selected"}), 200
+    except VariablePathNotDefined as e:
+        return jsonify({"ERROR":e.args}), 400
+    except Exception as e:
+        print(e)
+        return jsonify({"ERROR":"An error occured"}), 400
+
+@flask_app.route("/api/selector/os_type", methods=['GET'])
+def get_os_type():
+    try:
+        return jsonify(supported_systems.get_os_type())
+    except VariablePathNotDefined or PathDoesNotExist as e:
+        return jsonify({"ERROR":e.args}), 400
+    
+@flask_app.route("/api/selector/os_type", methods=['POST'])
+def post_os_type():
+    try:
+        os = request.get_json().get("os_type")
+        if not os:
+            return jsonify({"ERROR":"os type variable not found"})
+        else:
+            supported_systems.set_os_type(os)
+            return jsonify({"SUCCESS":"os type has been selected"}), 200
+    except VariablePathNotDefined as e:
+        return jsonify({"ERROR":e.args}), 400
+    except Exception as e:
+        print(e)
+        return jsonify({"ERROR":"An error occured"}), 400
+        
+@flask_app.route("/api/selector/os_version", methods=['GET'])
+def get_os_version():
+    try:
+        return jsonify(supported_systems.get_os_version())
+    except VariablePathNotDefined or PathDoesNotExist as e:
+        return jsonify({"ERROR":e.args}), 400
+
+@flask_app.route("/api/selector/os_version", methods=['POST'])
+def post_os_version():
+    try:
+        os = request.get_json().get("os_version")
+        if not os:
+            return jsonify({"ERROR":"os version variable not found"})
+        else:
+            supported_systems.set_os_version(os)
+            return jsonify({"SUCCESS":"os version has been selected"}), 200
+    except VariablePathNotDefined as e:
+        return jsonify({"ERROR":e.args}), 400
+    except Exception as e:
+        print(e)
+        return jsonify({"ERROR":"An error occured"}), 400
+    
+@flask_app.route("/api/questions", methods=['GET'])
+def get_questions():
+    try:
+        all_questions = []
+        for category in list_categories(supported_systems):
+            recommandations_in_cat = list_anssi_recommandations(category,supported_systems)
+            for level in recommandations_in_cat:
+                for recommandation in recommandations_in_cat[level]:
+                    split_name = recommandation.split("_")
+                    name = " ".join(split_name[1:])
+                    all_questions.append({"id":split_name[0],"name":name,"category":category,"level":level,"from":"ANSSI"})
+        return jsonify(all_questions), 200
+    except PathDoesNotExist as e:
+        return jsonify({"ERROR":e.args}), 400
+    except Exception as e:
+        print(e)
+        return jsonify({"ERROR":"An error occured"}), 400 
+        
+
+flask_app.run(host=SERVER_IP, port=SERVER_PORT, debug=True)
