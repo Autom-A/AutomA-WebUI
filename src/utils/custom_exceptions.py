a8980fc95cd346ab7e8e08bd4fe6c2c75d5f247d
Mijux <mijux-dev@proton.me>
2023-11-17 12:47:06 +0100

    add the submodule AutomA-Playbooks + ID management

--- src/utils/custom_exceptions.py
+++ src/utils/custom_exceptions.py
@@ -5,3 +5,9 @@ class PathDoesNotExist(Exception):
 
 class VariablePathNotDefined(Exception):
     pass
+
+class IDDoesNotExist(Exception):
+    pass
+
+class VariableIDNotDefined(Exception):
+    pass
