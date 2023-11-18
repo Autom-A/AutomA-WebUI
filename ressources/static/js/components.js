a62c6a4613101f2910bc67455bcc860e43f06835
Mijux <mijux-dev@proton.me>
2023-11-18 16:01:22 +0100

    Add Type Enum + Retrieve data from list<str> + add btn.onclick to retrieve data

--- ressources/static/js/components.js
+++ ressources/static/js/components.js
@@ -1,5 +1,11 @@
 var MATERIALIZE_FIFO = []
 
+const TYPES_QUESTION_ENUM = {
+    "str": 1,
+    "list<str>": 2,
+    "choice<str>": 3
+}
+
 function render_recommendations() {
     let recommendation_container = document.getElementById("recommendations-container")
 
@@ -78,7 +84,7 @@ function render_question(_id) {
             switch (todo.element_type) {
                 case "formselect":
                     let el = document.getElementById(todo.id)
-                    M.FormSelect.init(el,{})
+                    M.FormSelect.init(el, {})
                     break;
                 default:
                     break;
@@ -97,6 +103,35 @@ function render_question_btn(_id) {
     let btn = document.createElement("a")
     btn.classList.add(["btn-floating"])
 
+    btn.onclick = () => {
+        let question_data = JSON.parse(localStorage.getItem(_id))
+        let nb_questions = question_data.questions.length
+
+        for (let i = 0; i < nb_questions; i++) {
+            let input = document.getElementById(`${_id}-${i}`)
+            console.log(input.getAttribute("question-type"));
+            switch (parseInt(input.getAttribute("question-type"))) {
+                case TYPES_QUESTION_ENUM["str"]:
+                    break;
+                case TYPES_QUESTION_ENUM["list<str>"]:
+                    retrieve_list_str_data(input)
+                    break;
+                case TYPES_QUESTION_ENUM["choice<str>"]:
+                    break;
+                default:
+                    console.log("rien....");
+                    break;
+            }
+        }
+
+        let recommendation_line = document.getElementById(_id)
+        let radio_btn = recommendation_line.querySelector("#r-radio-btn")
+        radio_btn.innerText = "radio_button_checked"
+
+        let question_modal = document.getElementById(`q-${_id}`)
+        M.Modal.getInstance(question_modal).close()
+    }
+
     let icon = document.createElement("i")
     icon.classList.add(["material-icons"])
     icon.innerText = "check"
@@ -108,18 +143,16 @@ function render_question_btn(_id) {
 }
 
 function render_question_field(_id, index, one_field) {
-    return parse_type(one_field.type)(_id,index,one_field)
-}
-
-function parse_type(input_type){
-    switch (input_type) {
+    switch (one_field.type) {
         case "str":
-            return render_field_str
+            return render_field_str(_id, index, one_field)
         case "list<str>":
-            return render_field_list_str
+            return render_field_list_str(_id, index, one_field)
         default:
-            if (input_type.includes("choice<str>")) return render_field_choice_str
+            if (one_field.type.includes("choice<str>")) return render_field_choice_str(_id, index, one_field)
     }
+
+    console.log(`ERROR : the type : ${one_field.type} is not implemented`);
 }
 
 function render_field_str(_id, index, one_field) {
@@ -129,6 +162,7 @@ function render_field_str(_id, index, one_field) {
     let input = document.createElement("input")
     input.setAttribute("type", "text")
     input.setAttribute("id", `${_id}-${index}`)
+    input.setAttribute("question-type",TYPES_QUESTION_ENUM["str"])
 
     let label = document.createElement("label")
     label.innerText = one_field.title
@@ -140,26 +174,28 @@ function render_field_str(_id, index, one_field) {
     return div
 }
 
-function render_field_list_str(_id,index,one_field) {
+function render_field_list_str(_id, index, one_field) {
     let div_row = document.createElement("div")
     div_row.classList.add("row")
 
     let div_col = document.createElement("div")
-    div_col.classList.add(["input-field","col","s12"])
+    div_col.classList.add(["input-field", "col", "s12"])
 
     let title = document.createElement("p")
     title.classList.add(["caption"])
     title.innerText = one_field.title
 
     let chips = document.createElement("div")
-    chips.classList.add(["chips","chips-placeholder"])
+    chips.classList.add(["chips", "chips-placeholder"])
     chips.setAttribute("id", `${_id}-${index}`)
+    chips.setAttribute("question-type",TYPES_QUESTION_ENUM["list<str>"])
+
 
     let chips_opt = {
-        placeholder: `Enter ${one_field.name.replace("_"," ")}`,
-        secondaryPlaceholder: '+More',
+        placeholder: `Press enter to add ${one_field.name.replace("_", " ")}`,
+        secondaryPlaceholder: '+ More',
     }
-    M.Chips.init(chips,chips_opt)
+    M.Chips.init(chips, chips_opt)
 
     div_col.appendChild(title)
     div_col.appendChild(chips)
@@ -169,30 +205,32 @@ function render_field_list_str(_id,index,one_field) {
 }
 
 function render_field_choice_str(_id, index, one_field) {
-    let choices_to_parse = one_field.type.replace("choice<str>","") 
-    choices_to_parse = choices_to_parse.replace("[","").replace("]","")
+    let choices_to_parse = one_field.type.replace("choice<str>", "")
+    choices_to_parse = choices_to_parse.replace("[", "").replace("]", "")
     let choices_allowed = choices_to_parse.split(",")
 
     let div_row = document.createElement("div")
     div_row.classList.add(["row"])
 
     let div_col = document.createElement("div")
-    div_col.classList.add(["input-field","col", "s12"])
+    div_col.classList.add(["input-field", "col", "s12"])
 
     let select_choice = document.createElement("select")
-    select_choice.setAttribute("id",`${_id}-${index}`)
+    select_choice.setAttribute("id", `${_id}-${index}`)
+    select_choice.setAttribute("question-type",TYPES_QUESTION_ENUM["choice<str>"])
+
 
     let option_dis = document.createElement("option")
     option_dis.value = ""
-    option_dis.setAttribute('disabled','')
-    option_dis.setAttribute('selected','')
+    option_dis.setAttribute('disabled', '')
+    option_dis.setAttribute('selected', '')
     option_dis.innerText = "Choose your options"
 
     select_choice.appendChild(option_dis)
 
     for (let i = 0; i < choices_allowed.length; i++) {
         const one_choice = choices_allowed[i];
-        
+
         let option_choice = document.createElement("option")
         option_choice.value = one_choice
         option_choice.innerText = one_choice
@@ -204,15 +242,30 @@ function render_field_choice_str(_id, index, one_field) {
 
     let label = document.createElement("label")
     label.innerText = one_field.title
-    label.setAttribute("for",`${_id}-${index}`)
-    label.setAttribute("style","top:215px !important;") 
+    label.setAttribute("for", `${_id}-${index}`)
+    label.setAttribute("style", "top:215px !important;")
 
     div_col.appendChild(select_choice)
     div_col.appendChild(label)
 
     div_row.appendChild(div_col)
 
-    MATERIALIZE_FIFO.push({element_type:"formselect", id: select_choice.id})
+    MATERIALIZE_FIFO.push({ element_type: "formselect", id: select_choice.id })
 
     return div_row
+}
+
+function retrieve_list_str_data(input_component) {
+    let chip_instance = M.Chips.getInstance(input_component)
+
+    let data_to_save = []
+    chip_instance.chipsData.forEach(chip => {
+        data_to_save.push(chip.tag)
+    });
+
+    if (data_to_save.length > 0) {
+        localStorage.setItem(input_component.id, JSON.stringify(data_to_save))
+    } else {
+        localStorage.removeItem(input_component.id)
+    }
 }
