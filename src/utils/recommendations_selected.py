cd2cc2222f010697e803cfb3af7135b0873815d5
Mijux <mijux-dev@proton.me>
2023-11-20 11:10:16 +0100

    Implement in the back host inventory selection

--- src/utils/recommendations_selected.py
+++ src/utils/recommendations_selected.py
@@ -1,3 +1,7 @@
+from os.path import join
+from utils.custom_exceptions import MissingRecommendation
+
+from utils.supported_systems import SupportedSystems
 class SingletonRecommendationsSelected():
     """
     This class is a sigleton object for RecommendationSelected class
@@ -19,3 +23,13 @@ class RecommendationsSelected(SingletonRecommendationsSelected):
         super().__init__()
         if len(recommendations) > 0:
             self._recommendations_selected = recommendations
+
+    def write_yml(self):
+        if len(self._recommendations_selected) == 0:
+            raise MissingRecommendation("There must be at least one recommendation to apply")
+        supported_systems = SupportedSystems()
+        with open("./playbook.master.yml","w") as playbook_master_file:
+            playbook_master_file.write("---\n")
+            for recommendation in self._recommendations_selected:
+                recommendation_path = join(supported_systems._playbooks_location,recommendation)
+                playbook_master_file.write(f"- import_playbook: \"{recommendation_path}\"\n")
