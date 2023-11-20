c0440f3ce938bccebfb5341708f6f98c5908faa6
Mijux <mijux-dev@proton.me>
2023-11-20 23:21:13 +0100

    Host table rendering

--- ressources/static/js/app.init.js
+++ ressources/static/js/app.init.js
@@ -6,11 +6,21 @@ const SELECTED_ID_STORAGE = "SELECTED_ID_STORAGE"
 document.addEventListener('DOMContentLoaded', function () {
     let mod = document.getElementById("os-selector-modal")
     M.Modal.init(mod, {});
+
+    let tabs = document.getElementById("recommendation-inventory-tabs")
+    M.Tabs.init(tabs,{})
+
+    let inventory_form  = document.getElementById("inventory-input")
+    let select_inventory = inventory_form.querySelector("#select-connection")
+    select_inventory.onchange = fillAuthFromConnectionMethodValue
+    fillAuthFromConnectionMethodValue()
+
+    M.FormSelect.init(select_inventory, {})
+
     select_os_model_open()
 
     setTimeout(() => {
         document.getElementById("radio_button_unchecked").innerText = "radio_button_unchecked"
-        document.getElementById("navigate_next").innerText = "navigate_next"
     }, 250)
 });
 
@@ -23,4 +33,19 @@ function environment_selected_toggle() {
 function valid_environment() {
     get_recommendation_list()
     environment_selected_toggle()
+}
+
+function addHost() {
+    let hostItem = retrieveInventoryTableInput()
+    sendHost(hostItem)
+}
+
+function fillAuthFromConnectionMethodValue() {
+    let labelPasswordKeyfile = document.getElementById("label-password-keyfile")
+    let selectConnectionMethod = document.getElementById("select-connection")
+    if (selectConnectionMethod.value == "0") {
+        labelPasswordKeyfile.innerText = "Password"
+    } else if (selectConnectionMethod.value == "1") {
+        labelPasswordKeyfile.innerText = "Keyfile"
+    }
 }
